86a03530de485dff0665e8227431ac4fc9613620
nghuy285 <huyngo28052005@gmail.com>
2026-03-24 10:00:11 +0700

    Package Structure

---
+++ src/main/java/com/dev/kronix/schedular/SchedularApplication.java
@@ -0,0 +1,14 @@
+package com.dev.kronix.schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SchedularApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
